d30844e8088f8cd5532dbe9faee1eb56eb16a37a
shankhadeepmukherjee <shankhadeep.ju@gmail.com>
2020-02-15 00:56:27 +0530

    Add files via upload

--- egypt_prices.py
+++ egypt_prices.py
@@ -1,14 +1,20 @@
+#download rebar prices in Egypt
+#import important modules
 import pandas as pd
 import numpy as np
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
 import re
 
+#open the webpage
 url = "http://cementegypt.com/en/iron-prices/"
 html = urlopen(url)
+
+#read the html code and row items
 soup = bs(html, features="html.parser")
 all_links = soup.find_all('tr')
 
+#read the cell elements, strip unrequired items and write to a file
 list_rows = []
 
 for j in all_links:
@@ -22,4 +28,4 @@ df = df[0].str.split(',', expand = True)
 df[0] = df[0].str.strip('[')
 df[4] = df[4].str.strip(']')
 
-df.to_csv('05022020.csv')
+df.to_csv('07022020.csv') #change the file name to the date of running the file
